fdeb436fdb7a0fc749e92ca7fccb0adff7179bf0
Quentin Horgues <quentin.horgues@outlook.fr>
2021-12-21 13:54:17 +0100

    Test de la recuperation des coups d'une tour

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 int main(void)
 {
 	struct Board board = Init_Board();
-    move(&board, 3, 7, 4, 0);
+    move(&board, COOR( 0, 7), COOR(3, 4));
     print_board(&board, stdout);
 	
 	/* struct List_move list = init_list_move(COOR(0, 0));
@@ -14,13 +14,15 @@ int main(void)
 	list.push_back(&list, COOR(3, 4));
 	list.push_back(&list, COOR(7, 3));
 	list.push_back(&list, COOR(2, 4));
-	
+	*/
+	List_move list = init_list_move(COOR(3, 4));
+	get_list_move(&board, &list);
 	ptr_list_move ptr_list = list.list_move;
 	while (ptr_list != NULL)
 	{
 		printf("%d/%d\n", ptr_list->coor.x, ptr_list->coor.y);
 		ptr_list = ptr_list->next;
 	}
-	list.clear(&list); */
+	list.clear(&list);
     return 0;
 }
