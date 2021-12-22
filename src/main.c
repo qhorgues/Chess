6ed8ad9adf114a1073eafc7703e8eb83bad20bef
Quentin Horgues <quentin.horgues@outlook.fr>
2021-12-22 18:52:04 +0100

    Passage d'une double grille a une simple

--- src/main.c
+++ src/main.c
@@ -6,21 +6,14 @@
 int main(void)
 {
 	struct Board board = Init_Board();
-    move(&board, COOR( 0, 7), COOR(3, 4));
+    move(&board, off_set( 2, 7), off_set(3, 4));
     print_board(&board, stdout);
-	
-	/* struct List_move list = init_list_move(COOR(0, 0));
-	list.push_back(&list, COOR(2, 3));
-	list.push_back(&list, COOR(3, 4));
-	list.push_back(&list, COOR(7, 3));
-	list.push_back(&list, COOR(2, 4));
-	*/
-	List_move list = init_list_move(COOR(3, 4));
+	List_move list = init_list_move(off_set(3, 4));
 	get_list_move(&board, &list);
 	ptr_list_move ptr_list = list.list_move;
 	while (ptr_list != NULL)
 	{
-		printf("%d/%d\n", ptr_list->coor.x, ptr_list->coor.y);
+		printf("%d/%d\n", get_x(ptr_list->coor), get_y(ptr_list->coor));
 		ptr_list = ptr_list->next;
 	}
 	list.clear(&list);
