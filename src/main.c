caf8ef9e45c88393f337b16a1860d6e4c175f589
qhorgues <quentin.horgues@outlook.fr>
2021-12-24 11:55:09 +0100

    Test des mouvements du pion

--- src/main.c
+++ src/main.c
@@ -6,9 +6,11 @@
 int main(void)
 {
 	struct Board board = Init_Board();
-    move(&board, off_set( 2, 7), off_set(3, 4));
+    move(&board, off_set( 5, 1), off_set(5, 3));
+    move(&board, off_set( 4, 6), off_set(4, 3));
+	board.grid[off_set(5, 3)].prise_pass = true;
     print_board(&board, stdout);
-	List_move list = init_list_move(off_set(3, 4));
+	List_move list = init_list_move(off_set(4, 3));
 	get_list_move(&board, &list);
 	ptr_list_move ptr_list = list.list_move;
 	while (ptr_list != NULL)
@@ -17,5 +19,6 @@ int main(void)
 		ptr_list = ptr_list->next;
 	}
 	list.clear(&list);
+
     return 0;
 }
