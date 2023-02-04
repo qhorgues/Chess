cb8247139d68fcf3aa7ffe3d51d52ce9f398f8d3
Quentin Horgues <quentin.horgues@outlook.fr>
2023-02-04 16:12:09 +0100

    Creation du système de fichier log

---
+++ src/log/log.h
@@ -0,0 +1,31 @@
+#ifndef LOG_H
+#define LOG_H
+
+#include <stdbool.h>
+#include <errno.h>
+#include <string.h>
+
+typedef enum Error_type
+{
+    STATUS = 0,
+    WARN = 1,
+    ERROR = 2,
+    FATAL_ERROR = 3
+} Error_type;
+
+void _test(Error_type type_error, int condition, const char* str_condition, const char* message, int line, const char* file);
+#define test(type_error, condition, message) _test(type_error, condition, #condition, message, __LINE__, __FILE__)
+#define DEFAULT_MESSAGE strerror(errno)
+
+
+#ifndef NDEBUG
+    void open_log(void);
+    void logged(Error_type type, const char* format, ...);
+    void close_log(void);
+#else
+    #define open_log()
+    #define logged(type, str, ...)
+    #define close_log()
+#endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,5 +1,13 @@
+#include "log/log.h"
+
 
 int main(void)
 {
+    open_log();
+
+    int a = 12;
+    test(STATUS, a == 12, "");
+
+    close_log();
     return 0;
 }
